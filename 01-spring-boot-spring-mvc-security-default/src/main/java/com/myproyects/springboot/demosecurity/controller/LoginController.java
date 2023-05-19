93e30e6cb4be7003014cb3fd0fde895022276dc0
HugoSanche <107966476+HugoSanche@users.noreply.github.com>
2023-05-19 17:18:33 -0600

    Add files via upload

---
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/myproyects/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.myproyects.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/ShowMyLoginPage")
+    public String showMyLoginPage(){
+        //return "plain-login";
+        return "fancy-login";
+    }
+    @GetMapping("/access-denied")
+    public String accessDeny(){
+        //return "plain-login";
+        return "access-denied";
+    }
+}
--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/myproyects/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/myproyects/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,12 +2,54 @@ package com.myproyects.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    // add support for JDBC ... no more hardcoded users :-)
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id,pw, active from members where user_id=?"); // how get user in my database table
+        //define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?"); //how get roles en my database table
+
+        return
+                jdbcUserDetailsManager; //tell spring security to use JDBC authentication with our data source
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(form->
+                    form
+                            .loginPage("/ShowMyLoginPage") //controller Mapping
+                            .loginProcessingUrl("/authenticateTheUser")
+                            .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+        return http.build();
+    }
+}
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john= User.builder()
@@ -27,4 +69,22 @@ public class DemoSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
-}
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
